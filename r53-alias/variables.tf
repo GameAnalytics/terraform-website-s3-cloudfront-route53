c2511e2080bee6d043b0ecf0677ef7156775c0ee
Magnus Henoch <magnus@gameanalytics.com>
2023-01-10 10:54:58 +0000

    Add option for creating AAAA record in r53-alias module
    The site-main and site-redirect modules already support IPv6 (and
    r53-cname doesn't need to do anything special), so let's add the
    missing piece of the puzzle.

--- r53-alias/variables.tf
+++ r53-alias/variables.tf
@@ -13,3 +13,9 @@ variable "route53_zone_id" {
 variable "cdn_hosted_zone_id" {
   type = string
 }
+
+variable "ipv6" {
+  type        = bool
+  description = "Add AAAA alias record for IPv6 access to CloudFront distribution"
+  default     = false
+}
